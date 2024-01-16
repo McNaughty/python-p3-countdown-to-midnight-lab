ad89142cff5f9f256e696cab680d0a026c121f41
Winnie <wimacoj@gmail.com>
2024-01-16 17:30:41 +0300

    Completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,24 @@
 # your code goes here!
+
+def countdown(number):
+    counter = 1
+    while counter <= number:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        # counter +=1
+    else:
+        print("HAPPY NEW YEAR!")
+        
+    
+import time
+
+def countdown_with_sleep(number):
+    counter = 1
+    while counter <= number:
+        print(f'{number} SECOND(S)!')
+        time.sleep(1)  # Introduce a 1-second delay
+        number -= 1
+        # counter +=1
+    else:
+        print("HAPPY NEW YEAR!")
+    
